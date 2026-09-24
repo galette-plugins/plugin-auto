1abff538e6cbab6b0156686459184f969116f6ee
Johan Cwiklinski <johan@x-tnd.be>
2026-09-24 18:14:28 +0200

    Rely on core to create pictures directory

--- lib/GaletteAuto/Picture.php
+++ lib/GaletteAuto/Picture.php
@@ -10,7 +10,6 @@ declare(strict_types=1);
 
 namespace GaletteAuto;
 
-use Analog\Analog;
 use Galette\Core\Picture as GalettePicture;
 use Galette\Core\Plugins;
 
@@ -36,25 +35,6 @@ class Picture extends GalettePicture
     {
         $this->plugins = $plugins;
         $this->store_path = GALETTE_PHOTOS_PATH . '/auto_photos/';
-        if (!file_exists($this->store_path)) {
-            if (!mkdir($this->store_path)) {
-                Analog::log(
-                    'Unable to create photo dir `' . $this->store_path . '`.',
-                    Analog::ERROR
-                );
-            } else {
-                Analog::log(
-                    'New directory `' . $this->store_path . '` has been created',
-                    Analog::INFO
-                );
-            }
-        } elseif (!is_dir($this->store_path)) {
-            Analog::log(
-                'Unable to store plugin images, since `' . $this->store_path
-                . '` is not a directory.',
-                Analog::WARNING
-            );
-        }
         parent::__construct($id_adh);
     }
 
@@ -65,12 +45,12 @@ class Picture extends GalettePicture
      */
     protected function getDefaultPicture(): void
     {
-        $this->file_path = (string)realpath(
-            $this->plugins->getTemplatesPathFromName('Galette Auto')
-            . '/../../webroot/images/1f698.png'
-        );
         $this->format = 'png';
         $this->mime = 'image/png';
         $this->has_picture = false;
+        $this->setDefaultPath(
+            $this->plugins->getTemplatesPathFromName('Galette Auto')
+            . '/../../webroot/images/1f698.png'
+        );
     }
 }
